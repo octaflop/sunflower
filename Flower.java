7f8ff557d05a458d087204d8f4fc28365a70ef02
Faris Chebib <octaflop@gmail.com>
2011-04-16 16:27:53 -0700

    Small adjustments, boxes working

--- Flower.java
+++ Flower.java
@@ -1,64 +1,55 @@
 import processing.core.PApplet;
 public class Flower {
 	/**
-	 * A Flower is an agent which speaks and interprets
+	 * A Flower is an agent which speaks, interprets, and displays
 	 */
 	private static final long serialVersionUID = 1L;
-	float[] origin = new float[2];
-	float[] referent = new float[2];
+	int[] pcolor = new int[3];
+	int[] ncolor = new int[3];
+	int[] dispbox = new int[2];
+	int[] pos = new int[2];
 	PApplet parent;
 	
-	Flower(PApplet p, float originx, float originy, float referentx, float referenty) {
+	Flower(PApplet p) {
 		parent = p;
-		origin[0] = originx;
-		origin[1] = originy;
-		referent[0] = referentx;
-		referent[1] = referenty;
+		dispbox[0] = 75;
+		dispbox[1] = 75;
+		pcolor[0] = 50;
+		pcolor[1] = 50;
+		pcolor[2] = 50;
+	}
+	
+	void sizeit(int x, int y) {
+		dispbox[0] = x;
+		dispbox[1] = y;
+	}
+	
+	void positionit(int x, int y) {
+		pos[0] = x;
+		pos[1] = y;
+	}
+	
+	void colorit(int r, int g, int b) {
+		ncolor[0] = r;
+		ncolor[1] = g;
+		ncolor[2] = b;
 	}
 	
 	void display() {
-		parent.line(origin[0], origin[1], referent[0], referent[1]);
+		parent.fill(ncolor[0], ncolor[1], ncolor[2]);
+		parent.rect(pos[0], pos[1], dispbox[0], dispbox[1]);
 	}
 	
-	void update() {
-		// called just before display finishes
-		// TODO replace with something smarter
-		// april 5: replaced with annealing
-		float crt = 1;
-		for (int i = 0; i < 5; i++) {
-			float[] fixedref = new float[2];
-			fixedref[0] = parent.width/2;
-			fixedref[1] = parent.height/2;
-			referent = anneal(referent[0], referent[1], fixedref[0], fixedref[1]);
-		}
+	String speak() {
+		return "tets";
 	}
-	float[] anneal(float prefx, float prefy, float refx, float refy) {
-		float[] ret = new float[2];
-		float[] dif = new float[2];
-		dif[0] = PApplet.abs(prefx - refx);
-		dif[1] = PApplet.abs(prefx - refy);
-		float deltax = dif[0];
-		float deltay = dif[1];
-		if (dif[0] > 0) {
-			deltax = (float) (deltax * 0.8);
-		} 
-		else if (dif[0] < 0) {
-			deltax = (float) (deltax * 1.6);
-		} else {
-			deltax = 1;
-		}
-		
-		if (dif[1] > 0) {
-			deltay = (float) (deltay * 0.8);
-		} 
-		else if (dif[1] < 0) {
-			deltay = (float) (deltay * 1.6);
-		} else {
-			deltay = 1;
-		}
-		
-		ret[0] = prefx + parent.random(deltax, -deltax);
-		ret[1] = prefy + parent.random(deltay, -deltay);
-		return ret;
+	
+	void recolorit() {
+		// the interpreter class taking the previous colour and shoving it through the GA
+		ncolor[0] = (int) (ncolor[0] + parent.random(-30,30));
+		ncolor[1] = (int) (ncolor[1] + parent.random(-20,20));
+		ncolor[2] = (int) (ncolor[2] + parent.random(-10,10));
 	}
 }
+	
+
--- WorldLoop.java
+++ WorldLoop.java
@@ -7,59 +7,39 @@ public class WorldLoop extends PApplet {
 	private static final long serialVersionUID = 1L;
 	int width = 600;
 	int height = 600;
-	// first row of flowers
-	// TODO: Test these
-	Flower[] flowers1 = new Flower[2];
-	Flower[] flowers2 = new Flower[2];
 	//Referent referent = new Referent; //(this, width/2, height/2);
-	Referent referent;
-
+	Referent referent1 = new Referent(this, width/2, height/2);
+	Referent referent2 = new Referent(this, width/2, height/2);
+	int[] posa = new int[2];
+	int[] posb = new int[2];
+	int[] colora = new int[3];
+	int[] colorb = new int[3];
+	Flower flowera = new Flower(this);
+	Flower flowerb = new Flower(this);
+	
 	// The main setup function...
 	public void setup() {
 		size(width,height);
 		background(0);
+		posa[0] = width/2; // posa x
+		posa[1] = height/4; // posa y
+		posb[0] = width/2; // posa x
+		posb[1] = height/4 + height/2; // posa y
+		flowera.colorit(50, 50, 50);
+		flowera.positionit(posa[0], posa[1]);
+		flowerb.colorit(75, 75, 75);
+		flowerb.positionit(posb[0], posb[1]);
 		// initialize "sun"
-		Referent referent1 = new Referent(this, width/2, height/2);
-		Referent referent2 = new Referent(this, width/2, height/2);
 		// Initialize all flowers
-		for (int i =0; i< flowers1.length; i++) {
-			flowers1[i] = new Flower(this, 
-					// originx
-					(width/2 - width/4), 
-					// originy
-					(height/20 + (25*i)), 
-					// referentx
-					width/2,
-					// referenty
-					height/2
-				);
-			flowers2[i] = new Flower(this, 
-					// originx
-					(width/2 + width/4), 
-					// originy
-					(height/20 + (25*i)), 
-					// referentx
-					width/2, 
-					// referenty
-					height/2
-				);
-		}
 	}
 
 	public void draw() {
 		stroke(255);
 		if (mousePressed) {
-			fill(100,90,80);
-			referent.update();
-			referent.display();
-			for (int i =0; i< flowers1.length; i++) {
-				flowers1[i].update();
-				flowers2[i].update();
-				stroke(204,102,0);
-				flowers1[i].display();
-				stroke(153);
-				flowers2[i].display();
-			}
- 		}
+			flowera.display();
+			flowerb.display();
+			flowera.recolorit();
+			flowerb.recolorit();
+		}
 	}
 }
